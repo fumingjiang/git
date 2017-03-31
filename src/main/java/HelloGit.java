a5172c1e4554a3da8f09352e6ab733c3b0e2ad1f
傅明江 <fumj@udcredit.com>
2017-03-31 16:50:05 +0800

    gitclone 修改

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello ,git !");
         System.out.println("alter by git clone,thank you");
+        System.out.println("这是我的修改");
     }
 }
